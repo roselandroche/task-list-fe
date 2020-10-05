caacf0f3be0ae36d1b5c5b10b41ea548dc84016e
roselandroche <r.landroche@gmail.com>
2020-10-05 13:56:43 -0400

    Cleaning up and organizing

---
+++ my-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Task List</h1>
+        <p>
+          Create, update, and delete tasks.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
