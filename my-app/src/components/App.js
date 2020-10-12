c9dc9430029978032e72afae057d057b0b55cdac
roselandroche <r.landroche@gmail.com>
2020-10-12 13:32:40 -0700

    Data is being saved to local storage

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/App.css';
 
 import Header from './Header';
@@ -6,7 +6,18 @@ import TaskForm from './TaskForm';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  console.log(tasks)
+  useEffect(() => {
+    if (tasks === []) {
+      if (localStorage.getItem('tasks')) {
+        setTasks(JSON.parse(localStorage.getItem('tasks')))
+      }
+    } else {
+      if (localStorage.getItem('tasks')) {
+        localStorage.removeItem('tasks')
+      }
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }, [tasks])
   return (
     <div className="App">
       <header className="App-header">
