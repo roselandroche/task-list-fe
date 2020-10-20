155ad4d58e6e5da22126c1547255b4c53db69c07
roselandroche <r.landroche@gmail.com>
2020-10-20 11:05:01 -0700

    Finished basic styling

--- my-app/src/components/Task.js
+++ my-app/src/components/Task.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import '../css/Task.css';
 
 function Task({ task, toggle }) {
     
     return (
-        <>
+        <div className='ind_task'>
             <li onClick={() => toggle(task.id)}>
                 {task.completed ? <del>{`${task.taskName}`}</del> : `${task.taskName}`}
             </li>
-            <button>
+            <button className='edit_btn'>
                 <Link to={`/edittask/${task.id}`}>
                     Edit
                 </Link>
             </button>
-        </>
+        </div >
     )
 }
 
