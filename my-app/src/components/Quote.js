17fdf2374295877d076a9a574bcdbf89695dc5b7
roselandroche <r.landroche@gmail.com>
2020-11-06 16:46:39 -0800

    Added credit to API

--- my-app/src/components/Quote.js
+++ my-app/src/components/Quote.js
@@ -11,7 +11,6 @@ function Quote() {
             .then ( res => {
                 setQuote(res.data.contents.quotes[0].quote)
                 setAuthor(res.data.contents.quotes[0].author)
-                console.log(res.data.contents.quotes[0].author)
             })
             .catch ( err => {
                 console.log(err)
@@ -22,6 +21,12 @@ function Quote() {
         <div className='quote'>
             <h5>"{ quote }"</h5>
             <h6>- { author }</h6>
+            <span /* style="z-index:50;font-size:0.9em; font-weight: bold;"*/>
+                <img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/>
+                <a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" /* style="color: #ccc; margin-left: 4px; vertical-align: middle;" */>
+                    They Said So®
+                </a>
+            </span>
         </div>
     )
 }
