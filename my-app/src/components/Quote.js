c9ee4f66d081c3ec4c6182ba4883903e922398ff
roselandroche <r.landroche@gmail.com>
2020-11-06 16:41:38 -0800

    Added API call

--- my-app/src/components/Quote.js
+++ my-app/src/components/Quote.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Quote() {
-    const [quote, setQuote] = useState('')
-    
+    const [quote, setQuote] = useState('Default quote')
+    const [author, setAuthor] = useState('Anonymous')
+
     useEffect(() => {
-        
+        axios
+            .get('https://quotes.rest/qod/')
+            .then ( res => {
+                setQuote(res.data.contents.quotes[0].quote)
+                setAuthor(res.data.contents.quotes[0].author)
+                console.log(res.data.contents.quotes[0].author)
+            })
+            .catch ( err => {
+                console.log(err)
+            })
     }, [])
 
     return (
-        <div>
-            Insert quote here
+        <div className='quote'>
+            <h5>"{ quote }"</h5>
+            <h6>- { author }</h6>
         </div>
     )
 }
