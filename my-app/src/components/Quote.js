8eec7cdcd08dcdec8289eb097efbacc8c9b27692
roselandroche <r.landroche@gmail.com>
2020-11-06 16:49:00 -0800

    Minor edits, styling to come

--- my-app/src/components/Quote.js
+++ my-app/src/components/Quote.js
@@ -21,9 +21,9 @@ function Quote() {
         <div className='quote'>
             <h5>"{ quote }"</h5>
             <h6>- { author }</h6>
-            <span /* style="z-index:50;font-size:0.9em; font-weight: bold;"*/>
+            <span className='credit' /* style="z-index:50;font-size:0.9em; font-weight: bold;"*/>
                 <img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/>
-                <a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" /* style="color: #ccc; margin-left: 4px; vertical-align: middle;" */>
+                <a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" /* style="color: #ccc; margin-left: 4px; vertical-align: middle;" */ >
                     They Said So®
                 </a>
             </span>
